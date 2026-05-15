08fcf6988e60fbbe08ac8df3b18c6d4cc5d613c7
Adam <adampark123@Naver.com>
2026-05-15 16:27:16 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'scene-board-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // 크롤링으로 자동 업데이트되는 사이트이므로 항상 네트워크 우선
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
